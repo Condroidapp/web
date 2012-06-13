f91e7f61861ff723538998b24872268b2cc63a7d
Quinix <quinix@quinix.cz>
2012-06-13 22:47:12 +0200

    abilty for api call logging + fixed suport for X-If-Count-Not-Match

--- app/ApiModule/presenters/AnnotationsPresenter.php
+++ app/ApiModule/presenters/AnnotationsPresenter.php
@@ -27,8 +27,15 @@ class AnnotationsPresenter extends \FrontModule\BasePresenter  {
                 $this->getContext()->httpResponse->setCode(\Nette\Http\Response::S200_OK);
             } else {
                 $this->getContext()->httpResponse->setCode(\Nette\Http\Response::S304_NOT_MODIFIED);
-                dump($lastMod);
-                dump($newOnes);
+             //   dump($lastMod);
+             //   dump($newOnes);
+            }
+            $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
+            if($device != null && trim($device) != "") {
+                    $parts = explode(";", $device);
+                    $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
+                    $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
+                    $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`annotations-check`) VALUES ('".$model."','".$sn."',1) ON DUPLICATE KEY UPDATE `annotations-check`=`annotations-check`+1");
             }
             
             $this->terminate();
@@ -37,7 +44,33 @@ class AnnotationsPresenter extends \FrontModule\BasePresenter  {
         $data = $this->getContext()->database->table('annotations')->where("cid = ".$cid);
         if($this->getContext()->httpRequest->getHeader('If-Modified-Since',null) != null) {
             $date = new \Nette\DateTime($this->getContext()->httpRequest->getHeader('If-Modified-Since'));
-            $data->where('timestamp > ?',$date);
+            
+            $actualCount = $this->getContext()->database->table('annotations')->where('cid',$cid)->count();
+            $itemsCount = $this->getContext()->httpRequest->getHeader('X-If-Count-Not-Match',$actualCount);
+            $newOnes = $this->getContext()->database->table('annotations')->where('timestamp > ?',$lastMod)->where('cid',$cid)->count();
+            
+            $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
+            if($device != null && trim($device) != "") {
+                    $parts = explode(";", $device);
+                    $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
+                    $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
+                    $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`annotations-update`) VALUES ('".$model."','".$sn."',1) ON DUPLICATE KEY UPDATE `annotations-update`=`annotations-update`+1");
+            }
+            
+            if($newOnes > 0) {
+                $data->where('timestamp > ?',$date);
+            } elseif($actualCount == $itemsCount) {
+                $this->terminate();
+            }
+        }
+        else {
+            $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
+            if($device != null && trim($device) != "") {
+                    $parts = explode(";", $device);
+                    $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
+                    $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
+                    $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`annotations-full-download`) VALUES ('".$model."','".$sn."',1) ON DUPLICATE KEY UPDATE `annotations-full-download`=`annotations-full-download`+1");
+            }
         }
         
         $this->template->annotations = $data;
--- app/ApiModule/presenters/ConsPresenter.php
+++ app/ApiModule/presenters/ConsPresenter.php
@@ -13,6 +13,13 @@ namespace ApiModule;
 class ConsPresenter extends \FrontModule\BasePresenter {
     public function renderDefault() {
 		$this->template->cons = $this->getContext()->database->table('cons')->where("active=1")->limit(20)->order('id DESC');
+                $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
+                if($device != null && trim($device) != "") {
+                    $parts = explode(";", $device);
+                    $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
+                    $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
+                    $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`con-list`) VALUES ('".$model."','".$sn."',1) ON DUPLICATE KEY UPDATE `con-list`=`con-list`+1");
+                }
 	}
 }
 
