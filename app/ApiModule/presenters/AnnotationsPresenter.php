71013349e6a4452e206a22b8979934aa791a24c8
Jan Langer <jan.langer@gmail.com>
2014-01-29 21:48:33 +0100

    fixed last modified header check

--- app/ApiModule/presenters/AnnotationsPresenter.php
+++ app/ApiModule/presenters/AnnotationsPresenter.php
@@ -7,6 +7,7 @@ namespace ApiModule;
 use Kdyby\Doctrine\EntityDao;
 use Model\BasicFetchByQuery;
 use Model\Queries\AnnotationLastMod;
+use Nette\Application\BadRequestException;
 use Nette\Http\Request;
 use Nette\Http\Response;
 
@@ -20,33 +21,34 @@ class AnnotationsPresenter extends \FrontModule\BasePresenter  {
     /**
      * @autowire
      * @var \Nette\Http\Request */
-    public $httpRequest;
+    protected $httpRequest;
     /**
      * @autowire
-     * @var  \Nette\Http\Response
+     * @var \Nette\Http\Response
      */
-    public $httpResponse;
+    protected $httpResponse;
 
     /**
      * @autowire(\Model\Annotation, factory=\Kdyby\Doctrine\EntityDaoFactory)
      * @var \Kdyby\Doctrine\EntityDao
      */
-    private $annotationRepository;
-
-    protected function startup() {
-        parent::startup();
-        $this->annotationRepository = $this->context->annotationRepository;
-    }
+    protected $annotationRepository;
+    /**
+     * @autowire
+     * @var \Model\ApiLogger
+     */
+    //protected $apiLogger;
 
 
     public function actionDefault($cid) {
         if($cid <= 0) {
-            throw new \Nette\Application\BadRequestException('No cid supplied',404);
+            throw new BadRequestException('No cid supplied',404);
         }
         $clientLastMod = new \DateTime($this->httpRequest->getHeader('If-Modified-Since', "Sun, 13 Mar 1988 17:00:00 +0200")); // :-)
-        $clientLastMod=new \DateTime("Wed, 17 Apr 2013 21:40:44 +0200");
+
         $clientLastMod->setTimezone(new \DateTimeZone(date_default_timezone_get()));
 
+        /** @var $actualLastMod \DateTime */
         $actualLastMod = $this->annotationRepository->fetchOne(new AnnotationLastMod($cid))['timestamp'];
         $totalCount = count($this->annotationRepository->fetch(new BasicFetchByQuery(['event'=>$cid])));
         $clientCount = (int) $this->httpRequest->getHeader('X-If-Count-Not-Match', 0);
@@ -54,38 +56,48 @@ class AnnotationsPresenter extends \FrontModule\BasePresenter  {
         $data = NULL;
 
         if($totalCount === $clientCount && $actualLastMod <= $clientLastMod) {
-            $this->httpResponse->setCode(\Nette\Http\Response::S304_NOT_MODIFIED);
+            $this->httpResponse->setCode(Response::S304_NOT_MODIFIED);
+         //   $this->apiLogger->logCheck();
             $this->terminate();
         }
-        if($this->httpRequest->isMethod('HEAD')) {
-            $this->getContext()->httpResponse->setCode(\Nette\Http\Response::S200_OK);
-            $this->terminate();
+        else {
+            $this->httpResponse->setCode(Response::S200_OK);
+            if($this->httpRequest->isMethod('HEAD')) {
+              //  $this->apiLogger->logCheck();
+                $this->terminate();
+            }
         }
+
+        $this->httpResponse->setHeader('Last-Modified', $actualLastMod->format('D, j M Y H:i:s O'));
 
         if ($clientLastMod < $actualLastMod) {
-               $data = $this->annotationRepository->fetch(new BasicFetchByQuery(['event' => $cid, 'timestamp > ?'=> ($clientLastMod)]));
+            $data = $this->annotationRepository->fetch(new BasicFetchByQuery(['event' => $cid, 'timestamp > ?'=> ($clientLastMod)]));
+          //  $this->apiLogger->logUpdate();
         }
-        else if($totalCount !== $clientCount) {
+        else {
+
             $data = $this->annotationRepository->fetch(new BasicFetchByQuery(['event' => $cid]));
             $this->httpResponse->setHeader('X-Full-Update', 1);
-        } else {
-            throw new \Exception("This should not happen!");
+           // $this->apiLogger->logFullUpdate();
         }
 
 
-           /* $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
-            if($device != null && trim($device) != "") {
-                    $parts = explode(";", $device);
-                    if(isset($parts[0])) {
-                        if(\Nette\Utils\Strings::startsWith($parts[0], ":")) {
-                            $parts[0] = trim($parts[0], ": ");
-                        }
-                    }
-                    $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
-                    $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
-                    $os = $this->getContext()->database->quote((isset($parts[2])?$parts[2]:"unknown"));
-                    $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`annotations-check`,`os-version`) VALUES (".$model.",".$sn.",1,".$os.") ON DUPLICATE KEY UPDATE `os-version` = ".$os.", `annotations-check`=`annotations-check`+1");
-            }*/
+
+
+
+        /* $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
+         if($device != null && trim($device) != "") {
+                 $parts = explode(";", $device);
+                 if(isset($parts[0])) {
+                     if(\Nette\Utils\Strings::startsWith($parts[0], ":")) {
+                         $parts[0] = trim($parts[0], ": ");
+                     }
+                 }
+                 $model = $this->getContext()->database->quote((isset($parts[0])?$parts[0]:"unknown"));
+                 $sn = $this->getContext()->database->quote((isset($parts[1])?$parts[1]:"unknown"));
+                 $os = $this->getContext()->database->quote((isset($parts[2])?$parts[2]:"unknown"));
+                 $this->getContext()->database->exec("INSERT INTO apicalls (`device`,`serial`,`annotations-check`,`os-version`) VALUES (".$model.",".$sn.",1,".$os.") ON DUPLICATE KEY UPDATE `os-version` = ".$os.", `annotations-check`=`annotations-check`+1");
+         }*/
             
 
            /* $device = $this->getContext()->httpRequest->getHeader("X-Device-Info", null);
