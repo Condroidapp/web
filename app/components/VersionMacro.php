7d49324bc2416583e02a4496afdc10647a48b28b
Jan Langer <jan.langer@gmail.com>
2014-05-18 20:02:00 +0200

    added version macro

---
+++ app/components/VersionMacro.php
@@ -0,0 +1,31 @@
+<?php
+namespace App;
+
+use Latte\Compiler;
+use Latte\MacroNode;
+use Latte\Macros\MacroSet;
+use Latte\PhpWriter;
+use Nette\Utils\Random;
+
+class VersionMacro extends MacroSet
+{
+
+    public static function install(Compiler $compiler)
+    {
+        $me = new self($compiler);
+        $me->addMacro('version', array($me, 'macroVersion'));
+
+        return $me;
+    }
+
+    public function macroVersion(MacroNode $node, PhpWriter $writer)
+    {
+        $length = 10;
+        $word = $node->tokenizer->fetchWord();
+        if(is_numeric($word)) {
+            $length = (int) $word;
+        }
+        return $writer->write(' ?>?'.Random::generate($length).'<?php ');
+    }
+
+} 
