d67527e197c6eb5207c683022ecec52ece61f63b
Jan Langer <jan.langer@gmail.com>
2018-02-24 23:32:46 +0100

    Aaaad robot loader is back

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -11,7 +11,10 @@ if (\PHP_SAPI === 'cli' && in_array('--debug', $_SERVER['argv'], true)) {
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
-
+$configurator->createRobotLoader()
+	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../libs')
+	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
